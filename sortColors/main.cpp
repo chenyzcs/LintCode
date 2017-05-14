62a4430db3997476c66c8cf3ac44ed524d355c9e
zhou <chenyzcs@163.com>
2017-05-14 16:46:14 +0800

    sortColors

---
+++ sortColors/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void sortColors(vector<int> &nums) {
+	// write your code here
+	std::sort(nums.begin(), nums.end());
+}
+
+int main()
+{
+	return 0;
+}
